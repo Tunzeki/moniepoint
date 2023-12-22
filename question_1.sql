fa82d4da62da8adcaa760ce8a6eb749a211e0adf
Tunzeki <tundewebhead@gmail.com>
2023-12-22 11:11:17 +0100

    Write query for avg duration per trip on Saturdays

--- question_1.sql
+++ question_1.sql
@@ -83,26 +83,47 @@ HAVING
   AND toDayOfWeek(pickup_date) == 6 -- toDayOfWeek(date) assumes Monday as the first day of the week and represents it with 1, Tuesday with 2, ..., Saturday with 6, and Sunday with 7
 ORDER BY month_of_year ASC;
 
+-- Get monthly total trip duration for all Saturdays trips from Jan 01, 2014 to Dec 31, 2016
+SELECT
+  DATE_FORMAT(pickup_date, '%Y-%m') AS month_of_year, -- month_of_year
+  SUM(date_diff('second', pickup_datetime, dropoff_datetime)) AS total_trip_duration
+FROM tripdata 
+GROUP BY month_of_year 
+HAVING 
+  month_of_year BETWEEN '2014-01' AND '2016-12' 
+  AND toDayOfWeek(pickup_date) == 6 -- toDayOfWeek(date) assumes Monday as the first day of the week and represents it with 1, Tuesday with 2, ..., Saturday with 6, and Sunday with 7
+ORDER BY month_of_year ASC;
+
 
 /*
   Average number of trips on Saturdays = 
   total number of Saturday trips each month / number of Saturdays in the month
 
   Average fare (fare_amount) per trip on Saturdays = 
-  total fare for all Saturday trips in a month/ total number of Saturday trips that month
+  total fare for all Saturday trips in a month / total number of Saturday trips that month
+
+  Pick up time does not always start at 00 seconds, neither does drop_off time always end at 00 seconds
+  Therefore, using hour / minute as the unit in the date_diff function will cause over/underestimation 
+  of the trip duration, due to rounding off.
+  Use second as the unit in the date_diff function and 
+  convert average duration per trip to minute in the query output.
+  Average duration per trip on Saturdays =
+  total duration of all Saturday trips in a month / total number of Saturday trips that month
 */ 
 
 SELECT
   a.month_of_year,
   (a.sat_trip_count / b.number_of_sat_in_the_month) AS sat_mean_trip_count,
-  (a.total_fare / a.sat_trip_count) AS sat_mean_fare_per_trip
+  (a.total_fare / a.sat_trip_count) AS sat_mean_fare_per_trip,
+  (a.sat_total_trip_duration / a.sat_trip_count) / 60 AS sat_mean_duration_per_trip -- in minutes
 FROM 
 (
   -- Get monthly total number of trips on Saturdays from Jan 01, 2014 to Dec 31, 2016
   SELECT
       DATE_FORMAT(pickup_date, '%Y-%m') AS month_of_year, -- month_of_year
       COUNT(*) AS sat_trip_count,
-      SUM(fare_amount) AS total_fare
+      SUM(fare_amount) AS total_fare,
+      SUM(date_diff('second', pickup_datetime, dropoff_datetime)) AS sat_total_trip_duration
   FROM 
   tripdata 
   GROUP BY month_of_year 
